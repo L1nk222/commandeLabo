7e93866b6be77242c2af5d795fe795532d995d5c
Vincentime147 <bordarierkonigtheo@gmail.com>
2023-01-18 16:14:21 +0100

    Main fenetre

---
+++ src/views/FenetreMain.java
@@ -0,0 +1,17 @@
+package views;
+
+import javax.swing.*;
+
+public class FenetreMain {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField rechercheHome;
+    private JPanel Home;
+    private JPanel Stock;
+    private JPanel Historique;
+    private JPanel Commande;
+    private JTextField rechercheStock;
+    private JTable tableStock;
+    private JTextField rechercheHistorique;
+    private JTable tableHistorique;
+}
