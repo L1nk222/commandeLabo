1a14a19dc9a2554b33c5834b62f2406800b9e7b3
L1nk222 <antoinechalancon22@gmail.com>
2023-01-18 16:31:34 +0100

    ControllerStock 1.0

--- src/controller/ControllerStock.java
+++ src/controller/ControllerStock.java
@@ -1,6 +1,7 @@
 package controller;
 
 import DAO.DAOStock;
+import views.FenetreMain;
 
 import javax.swing.*;
 
@@ -8,11 +9,17 @@ public class ControllerStock {
 
     DAOStock daoStock;
     JPanel panelStock;
+    FenetreMain fenetre1;
 
-    public ControllerStock(DAOStock daoStock, JPanel panelStock){
+    public ControllerStock(DAOStock daoStock, JPanel panelStock, FenetreMain fenetre1){
         this.daoStock = daoStock;
         this.panelStock = panelStock;
+        this.fenetre1 = fenetre1;
 
 
     }
+    public void init(){
+        fenetre1.setVisible(true);
+    }
 }
+
--- src/views/FenetreMain.java
+++ src/views/FenetreMain.java
@@ -2,7 +2,7 @@ package views;
 
 import javax.swing.*;
 
-public class FenetreMain {
+public class FenetreMain extends JFrame {
     private JTabbedPane tabbedPane1;
     private JPanel panel1;
     private JTextField rechercheHome;
@@ -14,4 +14,115 @@ public class FenetreMain {
     private JTable tableStock;
     private JTextField rechercheHistorique;
     private JTable tableHistorique;
+
+    public JTabbedPane getTabbedPane1() {
+        return tabbedPane1;
+    }
+
+    public void setTabbedPane1(JTabbedPane tabbedPane1) {
+        this.tabbedPane1 = tabbedPane1;
+    }
+
+    public JPanel getPanel1() {
+        return panel1;
+    }
+
+    public void setPanel1(JPanel panel1) {
+        this.panel1 = panel1;
+    }
+
+    public JTextField getRechercheHome() {
+        return rechercheHome;
+    }
+
+    public void setRechercheHome(JTextField rechercheHome) {
+        this.rechercheHome = rechercheHome;
+    }
+
+    public JPanel getHome() {
+        return Home;
+    }
+
+    public void setHome(JPanel home) {
+        Home = home;
+    }
+
+    public JPanel getStock() {
+        return Stock;
+    }
+
+    public void setStock(JPanel stock) {
+        Stock = stock;
+    }
+
+    public JPanel getHistorique() {
+        return Historique;
+    }
+
+    public void setHistorique(JPanel historique) {
+        Historique = historique;
+    }
+
+    public JPanel getCommande() {
+        return Commande;
+    }
+
+    public void setCommande(JPanel commande) {
+        Commande = commande;
+    }
+
+    public JTextField getRechercheStock() {
+        return rechercheStock;
+    }
+
+    public void setRechercheStock(JTextField rechercheStock) {
+        this.rechercheStock = rechercheStock;
+    }
+
+    public JTable getTableStock() {
+        return tableStock;
+    }
+
+    public void setTableStock(JTable tableStock) {
+        this.tableStock = tableStock;
+    }
+
+    public JTextField getRechercheHistorique() {
+        return rechercheHistorique;
+    }
+
+    public void setRechercheHistorique(JTextField rechercheHistorique) {
+        this.rechercheHistorique = rechercheHistorique;
+    }
+
+    public JTable getTableHistorique() {
+        return tableHistorique;
+    }
+
+    public void setTableHistorique(JTable tableHistorique) {
+        this.tableHistorique = tableHistorique;
+    }
+
+    public  FenetreMain(){
+        tabbedPane1.setSelectedComponent(Stock);
+        setContentPane(tabbedPane1);
+        setExtendedState(FenetreLab.MAXIMIZED_BOTH);
+
+
+
+
+
+    }
+    public void home(){
+        setContentPane(Home);
+    }
+    public void Stock(){
+        tabbedPane1.setSelectedComponent(Stock);
+        setContentPane(tabbedPane1);
+    }
+    public void Commande(){
+        tabbedPane1.setSelectedComponent(Commande);
+        setContentPane(tabbedPane1);
+
+    }
 }
